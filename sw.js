cf6e58a4dfb460bd8b79544a9262b16a95ee83b8
thebigmarty33127-create <thebigmarty33127@gmail.com>
2026-04-29 17:12:20 -1000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', e => {
+    e.waitUntil(
+      caches.open('coach-cache').then(cache => {
+        return cache.addAll([
+          './',
+          './index.html'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', e => {
+    e.respondWith(
+      caches.match(e.request).then(response => response || fetch(e.request))
+    );
+  });
